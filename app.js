37826bd709796da4009341e36414d7e04ec3d51b
nabindhami14 <nabindhami4747@gmail.com>
2024-07-19 10:35:45 +0545

    feat: added server

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
-console.log("Hello World!!");
+const express = require("express");
 
+const app = express();
 
+app.get("/", (req, res) => {
+  res.send("HELLO WORLD FROM THE SERVER!!");
+});
+
+app.listen(3000, () => console.log("SERVING"));
